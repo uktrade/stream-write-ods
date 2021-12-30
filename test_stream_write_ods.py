193ebfdbd7f1a5f9cf4530b45edc40068fe0212a
Michal Charemza <michal@charemza.name>
2021-12-30 10:43:06 +0000

    tests: assert on basic functionality

--- test_stream_write_ods.py
+++ test_stream_write_ods.py
@@ -1,6 +1,9 @@
+from collections import OrderedDict
+from io import BytesIO
+from pyexcel_odsr import get_data
 from stream_write_ods import stream_write_ods
 
 
 def test_stream_write_ods():
-    ods = b''.join(stream_write_ods())
-    assert isinstance(ods, bytes)
+    data = get_data(BytesIO(b''.join(stream_write_ods())))
+    assert data == OrderedDict([('Sheet1', [['Col 1', 'Col 2']])])
