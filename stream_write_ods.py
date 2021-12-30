1cf95de70f06598c0ced10cbc5fb2231e86dfdb3
Michal Charemza <michal@charemza.name>
2021-12-30 12:19:28 +0000

    refactor: hard coded data in the order output

--- stream_write_ods.py
+++ stream_write_ods.py
@@ -6,14 +6,14 @@ def stream_write_ods():
     modified_at = datetime.now()
     perms = 0o600
 
+    mimetype = \
+        'application/vnd.oasis.opendocument.spreadsheet'
     manifest = \
         '<?xml version="1.0" encoding="UTF-8"?>' \
         '<manifest:manifest xmlns:manifest="urn:oasis:names:tc:opendocument:xmlns:manifest:1.0" manifest:version="1.2">' \
             '<manifest:file-entry manifest:full-path="/" manifest:version="1.2" manifest:media-type="application/vnd.oasis.opendocument.spreadsheet"/>' \
             '<manifest:file-entry manifest:full-path="content.xml" manifest:media-type="text/xml"/>' \
         '</manifest:manifest>'
-    mimetype = \
-        'application/vnd.oasis.opendocument.spreadsheet'
     content = \
         '<?xml version="1.0" encoding="UTF-8"?>' \
         '<office:document-content xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" office:version="1.2">' \
