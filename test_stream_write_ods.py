0f054fd4fcba113e47b4ee2ddfa760743d8ea381
Michal Charemza <michal@charemza.name>
2021-12-30 13:53:48 +0000

    tests: ensure has correct magic values

--- test_stream_write_ods.py
+++ test_stream_write_ods.py
@@ -5,10 +5,19 @@ from pandas_ods_reader import read_ods
 from stream_write_ods import stream_write_ods
 
 
-def test_stream_write_ods():
+def test_openable_with_pandas():
     with NamedTemporaryFile() as f:
         f.write(b''.join(stream_write_ods()))
         f.flush()
         data = list(read_ods(f.name, "Sheet 1").T.to_dict().values())
 
     assert data == [{'Col 1': 'Value 1', 'Col 2': 'Value 2'}]
+
+
+def test_has_correct_magic_values():
+    data = b''.join(stream_write_ods())
+
+    # Per http://docs.oasis-open.org/office/v1.2/os/OpenDocument-v1.2-os-part3.odt
+    assert data[0:2] == b'PK'
+    assert data[30:38] == b'mimetype'
+    assert data[38:84] == b'application/vnd.oasis.opendocument.spreadsheet'
