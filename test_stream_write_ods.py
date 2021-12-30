34e7edb53ce39243749b29cb46a11d1bd3d0b261
Michal Charemza <michal@charemza.name>
2021-12-30 12:15:56 +0000

    tests: choose specific sheet

--- test_stream_write_ods.py
+++ test_stream_write_ods.py
@@ -9,6 +9,6 @@ def test_stream_write_ods():
     with NamedTemporaryFile() as f:
         f.write(b''.join(stream_write_ods()))
         f.flush()
-        data = list(read_ods(f.name).T.to_dict().values())
+        data = list(read_ods(f.name, "Sheet 1").T.to_dict().values())
 
     assert data == [{'Col 1': 'Value 1', 'Col 2': 'Value 2'}]
