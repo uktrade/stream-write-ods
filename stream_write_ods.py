bf0c1f0088a43b925d97ac1abb7a3894b1dfd9da
Michal Charemza <michal@charemza.name>
2021-12-30 17:48:45 +0000

    feat: create sheet based on passed argument

--- stream_write_ods.py
+++ stream_write_ods.py
@@ -1,9 +1,11 @@
 from codecs import iterencode
 from datetime import datetime
+from xml.sax.saxutils import escape, quoteattr
+
 from stream_zip import ZIP, NO_COMPRESSION, stream_zip
 
 
-def stream_write_ods():
+def stream_write_ods(sheets):
 
     def files():
         modified_at = datetime.now()
@@ -27,19 +29,21 @@ def stream_write_ods():
             yield '<office:document-content xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" office:version="1.2">'
             yield '<office:body>'
             yield '<office:spreadsheet>'
-            yield '<table:table table:name="Sheet 1">'
-            yield '<table:table-column table:number-columns-repeated="2"/>'
-            yield '<table:table-header-rows>'
-            yield '<table:table-row>'
-            yield '<table:table-cell office:value-type="string"><text:p>Col 1</text:p></table:table-cell>'
-            yield '<table:table-cell office:value-type="string"><text:p>Col 2</text:p></table:table-cell>'
-            yield '</table:table-row>'
-            yield '</table:table-header-rows>'
-            yield '<table:table-row>'
-            yield '<table:table-cell office:value-type="string"><text:p>Value 1</text:p></table:table-cell>'
-            yield '<table:table-cell office:value-type="string"><text:p>Value 2</text:p></table:table-cell>'
-            yield '</table:table-row>'
-            yield '</table:table>'
+            for name, columns, rows in sheets:
+                yield f'<table:table table:name={quoteattr(name)}>'
+                yield f'<table:table-column table:number-columns-repeated={quoteattr(str(len(columns)))}/>'
+                yield '<table:table-header-rows>'
+                yield '<table:table-row>'
+                for column in columns:
+                    yield f'<table:table-cell office:value-type="string"><text:p>{escape(column)}</text:p></table:table-cell>'
+                yield '</table:table-row>'
+                yield '</table:table-header-rows>'
+                for row in rows:
+                    yield '<table:table-row>'
+                    for value in row:
+                        yield f'<table:table-cell office:value-type="string"><text:p>{escape(value)}</text:p></table:table-cell>'
+                    yield '</table:table-row>'
+                yield '</table:table>'
             yield '</office:spreadsheet>'
             yield '</office:body>'
             yield '</office:document-content>'
--- test_stream_write_ods.py
+++ test_stream_write_ods.py
@@ -4,17 +4,43 @@ from pandas_ods_reader import read_ods
 from stream_write_ods import stream_write_ods
 
 
+def get_sheets():
+    def get_sheet_1_rows():
+        yield 'Row 1 Column 1', 'Row 1 Column 2'
+        yield 'Row 2 Column 1', 'Row 2 Column 2'
+
+    yield 'Sheet 1', ('Column 1', 'Column 2'), get_sheet_1_rows()
+
+    def get_sheet_2_rows():
+        yield 'Row 1 Column 1',
+
+    yield 'Sheet 2', ('Column 1',), get_sheet_2_rows()
+
+
 def test_openable_with_pandas():
     with NamedTemporaryFile() as f:
-        f.write(b''.join(stream_write_ods()))
+        f.write(b''.join(stream_write_ods(get_sheets())))
         f.flush()
-        data = list(read_ods(f.name, "Sheet 1").T.to_dict().values())
+        sheet_1 = read_ods(f.name, "Sheet 1")
+        sheet_1_rows = sheet_1.values.tolist()
+        sheet_1_cols =  sheet_1.columns.tolist()
+        sheet_2 = read_ods(f.name, "Sheet 2")
+        sheet_2_rows = sheet_2.values.tolist()
+        sheet_2_cols =  sheet_2.columns.tolist()
 
-    assert data == [{'Col 1': 'Value 1', 'Col 2': 'Value 2'}]
+    assert sheet_1_rows == [
+        ['Row 1 Column 1', 'Row 1 Column 2'],
+        ['Row 2 Column 1', 'Row 2 Column 2'],
+    ]
+    assert sheet_1_cols == ['Column 1', 'Column 2']
+    assert sheet_2_rows == [
+        ['Row 1 Column 1',]
+    ]
+    assert sheet_2_cols == ['Column 1',]
 
 
 def test_has_correct_magic_values():
-    data = b''.join(stream_write_ods())
+    data = b''.join(stream_write_ods(get_sheets()))
 
     # Per http://docs.oasis-open.org/office/v1.2/os/OpenDocument-v1.2-os-part3.odt
     assert data[0:2] == b'PK'
