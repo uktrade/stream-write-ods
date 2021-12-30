e779f8b2fc3237524746b3ce44423c2b436733ec
Michal Charemza <michal@charemza.name>
2021-12-30 17:17:10 +0000

    refactor: put content xml in generator

--- stream_write_ods.py
+++ stream_write_ods.py
@@ -19,27 +19,30 @@ def stream_write_ods():
                 b'<manifest:file-entry manifest:full-path="content.xml" manifest:media-type="text/xml"/>' \
             b'</manifest:manifest>',
         )
-        yield 'content.xml', modified_at, perms, ZIP, (
-            b'<?xml version="1.0" encoding="UTF-8"?>' \
-            b'<office:document-content xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" office:version="1.2">' \
-                b'<office:body>' \
-                    b'<office:spreadsheet>' \
-                        b'<table:table table:name="Sheet 1">' \
-                            b'<table:table-column table:number-columns-repeated="2"/>' \
-                            b'<table:table-header-rows>' \
+
+        def content_xml():
+            yield \
+                b'<?xml version="1.0" encoding="UTF-8"?>' \
+                b'<office:document-content xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" office:version="1.2">' \
+                    b'<office:body>' \
+                        b'<office:spreadsheet>' \
+                            b'<table:table table:name="Sheet 1">' \
+                                b'<table:table-column table:number-columns-repeated="2"/>' \
+                                b'<table:table-header-rows>' \
+                                    b'<table:table-row>' \
+                                        b'<table:table-cell office:value-type="string"><text:p>Col 1</text:p></table:table-cell>' \
+                                        b'<table:table-cell office:value-type="string"><text:p>Col 2</text:p></table:table-cell>' \
+                                    b'</table:table-row>' \
+                                b'</table:table-header-rows>' \
                                 b'<table:table-row>' \
-                                    b'<table:table-cell office:value-type="string"><text:p>Col 1</text:p></table:table-cell>' \
-                                    b'<table:table-cell office:value-type="string"><text:p>Col 2</text:p></table:table-cell>' \
+                                    b'<table:table-cell office:value-type="string"><text:p>Value 1</text:p></table:table-cell>' \
+                                    b'<table:table-cell office:value-type="string"><text:p>Value 2</text:p></table:table-cell>' \
                                 b'</table:table-row>' \
-                            b'</table:table-header-rows>' \
-                            b'<table:table-row>' \
-                                b'<table:table-cell office:value-type="string"><text:p>Value 1</text:p></table:table-cell>' \
-                                b'<table:table-cell office:value-type="string"><text:p>Value 2</text:p></table:table-cell>' \
-                            b'</table:table-row>' \
-                        b'</table:table>' \
-                    b'</office:spreadsheet>' \
-                b'</office:body>' \
-            b'</office:document-content>',
-        )
+                            b'</table:table>' \
+                        b'</office:spreadsheet>' \
+                    b'</office:body>' \
+                b'</office:document-content>'
+
+        yield 'content.xml', modified_at, perms, ZIP, content_xml()
 
     yield from stream_zip(files())
