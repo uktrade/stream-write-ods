2c73279b67ab6272c5a185130f9c9fd6bad793a4
Michal Charemza <michal@charemza.name>
2021-12-30 11:02:39 +0000

    feat: ensure table-row are all in table-rows

--- test_stream_write_ods.py
+++ test_stream_write_ods.py
@@ -1,9 +1,14 @@
 from collections import OrderedDict
 from io import BytesIO
-from pyexcel_odsr import get_data
+from tempfile import NamedTemporaryFile
+from pandas_ods_reader import read_ods
 from stream_write_ods import stream_write_ods
 
 
 def test_stream_write_ods():
-    data = get_data(BytesIO(b''.join(stream_write_ods())))
-    assert data == OrderedDict([('Sheet 1', [['Col 1', 'Col 2']])])
+    with NamedTemporaryFile() as f:
+        f.write(b''.join(stream_write_ods()))
+        f.flush()
+        data = list(read_ods(f.name).T.to_dict().values())
+
+    assert data == [{'Col 1': 'Value 1', 'Col 2': 'Value 2'}]
