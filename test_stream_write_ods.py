4fc0549f6728fd1454c7517d100a0ea70a6b1169
Michal Charemza <michal@charemza.name>
2022-01-05 05:55:42 +0000

    tests: add paranoia check that <![CDATA[ is escaped

--- test_stream_write_ods.py
+++ test_stream_write_ods.py
@@ -21,6 +21,7 @@ def get_sheets():
         yield True,
         yield b'Binary data',
         yield None,
+        yield '<![CDATA[',
 
     yield 'Sheet 2', ('Column 1',), get_sheet_2_rows()
 
@@ -50,6 +51,7 @@ def test_openable_with_pandas():
         [True,],
         ['QmluYXJ5IGRhdGE=',],
         ['#N/A',],
+        ['<![CDATA[',]
     ]
     assert sheet_2_cols == ['Column 1',]
 
